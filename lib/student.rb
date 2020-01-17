b4984b0f4c924486ea7eaa954c6d2995aa9b16ae
Michael Newman <coloradohokie@gmail.com>
2020-01-17 13:21:04 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,18 +3,56 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    student = self.new
+    student.id = row[0]
+    student.name = row[1]
+    student.grade = row[2]
+    student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students
+    SQL
+    
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE name LIKE ?
+    SQL
+    # binding.pry
+    row = DB[:conn].execute(sql, name).first
+    self.new_from_db(row)
   end
-  
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+     SELECT * FROM students WHERE grade LIKE '9%'
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.students_below_12th_grade
+    ### Can do this using sql or ruby. What is the best way?? Ask instructor.
+
+      all.select {|student| student.grade != "12"}
+  end
+
+  def self.first_X_students_in_grade_10(num_students)
+    all.select {|student| student.grade == "10"}.first(num_students)
+  end
+
+  def self.first_student_in_grade_10
+    first_X_students_in_grade_10(1)[0]
+    
+  end
+  def self.all_students_in_grade_X(grade)
+    all.select {|student| student.grade.to_i == grade}
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade) 
